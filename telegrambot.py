db4e4ed5e50c3496d3ef077aebd4a254ef1a2c5c
Nesan02 <71171811+Nesan02@users.noreply.github.com>
2020-09-14 17:11:20 +0530

    Update telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -3,19 +3,20 @@ x =" "  #ENTER ADAFRUIT_IO_USERNAME
 y = " "  #ENTER ADAFRUIT_IO_KEY
 from Adafruit_IO import Client, Feed
 aio = Client(x,y)
-result = aio.create_feed(new)
+feed=Feed(name='bot')
+result = aio.create_feed(feed)
 from Adafruit_IO import Data
 !pip install python-telegram-bot
 from Adafruit_IO import Client,Data
 from telegram.ext import Updater,CommandHandler
 def on(bot,update):
   chat_id = update.message.chat_id    
-  aio.create_data('bot123',Data(value = 1))
+  aio.create_data('bot',Data(value = 1))
   bot.send_message(chat_id =chat_id,text ="Lights On")
 
 def off(bot,update):
   chat_id = update.message.chat_id
-  aio.create_data('bot123',Data(value = 0))
+  aio.create_data('bot',Data(value = 0))
   bot.send_message(chat_id =chat_id,text ="Lights Off")
 
 updater = Updater('1294830385:AAF_zy888DPBL2oIM9nvfQZYNPOMwWEm5CY')     #Use Telegram Token HTTP API
